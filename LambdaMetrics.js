b8ded2932ae46cad08048bce64ccd06bd5104512
Kaushik R Bangera <50287102+NeuronEnix@users.noreply.github.com>
2022-03-08 08:44:25 +0530

    Added: timers + cache only one instance

--- LambdaMetrics.js
+++ LambdaMetrics.js
@@ -3,10 +3,13 @@ const moment = require( "moment" );
 class LambdaMetrics {
   #instName; #invokeType; #invokeCount;
   #inTime; #outTime; #totalTime;
+  #timerObj;
+
   constructor() {
     this.#instName = moment().format( "HH:mm:ss:SSS" );
     this.#invokeCount = 0;
     this.#invokeType = "COLD START";
+    this.#timerObj = {};
   }
 
   invokeType() { return this.#invokeType };
@@ -14,13 +17,14 @@ class LambdaMetrics {
   start() {
     this.#inTime = moment();
     this.#invokeCount += 1;
+    this.#timerObj = {};
     return this;
   }
 
   end() {
     const curTime = moment();
-    this.#totalTime = curTime.diff( this.#inTime );
-    this.#inTime = this.#inTime.format( "HH:mm:ss:SSS" );
+    this.#totalTime = moment.isMoment( this.#inTime ) ? curTime.diff( this.#inTime ) : "N/A";
+    this.#inTime = moment.isMoment( this.#inTime ) ? this.#inTime.format( "HH:mm:ss:SSS" ) : "N/A";
     this.#outTime = curTime.format( "HH:mm:ss:SSS" );
     return this;
   }
@@ -33,10 +37,23 @@ class LambdaMetrics {
       inTime: this.#inTime,
       outTime: this.#outTime,
       totalTime: this.#totalTime,
+      timers: this.#timerObj,
     };
+
     this.#invokeType = "WARM START";
     return metricsData;
   }
+  startTimer( timerID ) {
+    this.#timerObj[ timerID ] = moment();
+    return this;
+  }
+  endTimer( timerID ) {
+    if ( moment.isMoment( this.#timerObj[ timerID ] ) )
+      this.#timerObj[ timerID ] = moment().diff( this.#timerObj[ timerID ] );
+    return this;
+  }
 }
 
-module.exports = { LambdaMetrics };
+const lambdaMetricsInstance = new LambdaMetrics();
+
+module.exports = { lambdaMetricsInstance };
