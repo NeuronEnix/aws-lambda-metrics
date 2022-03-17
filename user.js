4611ee34b35b574f9b14f30e937f4686ce7aab53
Kaushik R Bangera <50287102+NeuronEnix@users.noreply.github.com>
2022-03-17 21:36:55 +0530

    Removed: timers

--- user.js
+++ user.js
@@ -3,13 +3,8 @@ const { initUserModel } = require("./userModel");
 
 async function userList() {
 
-  lambdaMetrics.startTimer("user.initModel");
   const userModel = await initUserModel( lambdaMetrics );
-  lambdaMetrics.endTimer("user.initModel");
-
-  lambdaMetrics.startTimer("user.getUserFromDB");
   const userList_DB = await userModel.sequelize.query( "SELECT * FROM user", { raw: true, nest: true });
-  lambdaMetrics.endTimer("user.getUserFromDB");
 
   return userList_DB;
 }
