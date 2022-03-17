094e2d63b3fe33ad1d09dd9e47520bcd966d5bd6
Kaushik R Bangera <50287102+NeuronEnix@users.noreply.github.com>
2022-03-17 20:45:22 +0530

    Added: userGetDetail()

--- user.js
+++ user.js
@@ -8,10 +8,18 @@ async function userList() {
   lambdaMetrics.endTimer("user.initModel");
 
   lambdaMetrics.startTimer("user.getUserFromDB");
-  const userList_DB = await userModel.sequelize.query( "SELECT * FROM user", {raw:true, nest: true });
+  const userList_DB = await userModel.sequelize.query( "SELECT * FROM user", { raw: true, nest: true });
   lambdaMetrics.endTimer("user.getUserFromDB");
 
   return userList_DB;
 }
 
-module.exports = { userList };
+async function userGetDetail( userId ) {
+
+  const userModel = await initUserModel( lambdaMetrics );
+  const userGetDetail_DB = await userModel.sequelize.query( `SELECT * FROM user WHERE user_id = ${ userId }`, { raw: true, nest: true });
+
+  return userGetDetail_DB;
+}
+
+module.exports = { userList, userGetDetail };
